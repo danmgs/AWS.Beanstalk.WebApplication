b5e7be92f13674010784b541d1a9605674673f2e
Daniel <dnlnguyen@gmail.com>
2020-02-17 13:43:19 +0100

    edit readme + fix pack.bat
    pack.bat : wwwroot directory was missing (website not having js and css files)

--- AWS.Beanstalk.WebApplication/scripts/pack.bat
+++ AWS.Beanstalk.WebApplication/scripts/pack.bat
@@ -2,9 +2,11 @@ md output
 dotnet publish ..\AWS.Beanstalk.WebApplication.csproj -o .\output\site
 pushd output
 pushd site
-"C:\Program Files\7-Zip\7z.exe" a ../site.zip *.*
+move .ebextensions ..\
+pause
+"C:\Program Files\7-Zip\7z.exe" -tzip a ../site.zip .
 popd
-move .\site\.ebextensions .
+pause
 copy ..\aws-windows-deployment-manifest.json .
 "C:\Program Files\7-Zip\7z.exe" -tzip a bundle.zip . -x!site
 pause
